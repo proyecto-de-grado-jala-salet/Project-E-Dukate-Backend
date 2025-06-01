b1cfef53d3cedb73daa8085150a45fb1ef1a6c59
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2025-06-01 12:05:02 -0400

    feat: add search functionality to the schedule

--- E-Dukate.Domain/Entities/Schedules/TimeSlot.cs
+++ E-Dukate.Domain/Entities/Schedules/TimeSlot.cs
@@ -1,7 +1,11 @@
+using E_Dukate.Domain.Primitives;
+
 namespace E_Dukate.Domain.Entities.Schedules;
 
-public class TimeSlot
+public class TimeSlot : Entity
 {
+    public Guid ScheduleId { get; set; }
+    public Schedule? Schedule { get; set; }
     public TimeOnly StartTime { get; set; }
     public TimeOnly EndTime { get; set; }
 }
