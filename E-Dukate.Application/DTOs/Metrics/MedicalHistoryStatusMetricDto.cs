11457df566639a7d04774bd1310f76405cf0a67d
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2025-06-19 04:04:19 -0400

    feat: add process metrics logic of result

---
+++ E-Dukate.Application/DTOs/Metrics/MedicalHistoryStatusMetricDto.cs
@@ -0,0 +1,7 @@
+namespace E_Dukate.Application.DTOs.Metrics;
+
+public class MedicalHistoryStatusMetricDto
+{
+    public string Status { get; set; } = string.Empty;
+    public int Count { get; set; }
+}
